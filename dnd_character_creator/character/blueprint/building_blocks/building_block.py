ef3ee57548575bdd62d72cac644d6c5343fe12c3
Tesla2000 <fratajczak124@gmail.com>
2025-12-08 20:26:51 +0100

    Updated building_block.py to simplify subclass creation

--- dnd_character_creator/character/blueprint/building_blocks/building_block.py
+++ dnd_character_creator/character/blueprint/building_blocks/building_block.py
@@ -12,7 +12,6 @@ from dnd_character_creator.character.blueprint.blueprint import Blueprint
 class BuildingBlock(BaseModel, ABC):
     model_config = ConfigDict(frozen=True)
 
-    @abstractmethod
     def get_change(
         self, blueprint: Blueprint
     ) -> Generator[Blueprint, Blueprint, None]:
@@ -21,6 +20,18 @@ class BuildingBlock(BaseModel, ABC):
         Receives the current blueprint state from yield returns,
         allowing decisions based on current state.
         """
+        yield self._get_change(blueprint)
+
+    @abstractmethod
+    def _get_change(
+        self, blueprint: Blueprint
+    ) -> Blueprint:
+        """Returns Blueprint differences to apply.
+
+        Receives the current blueprint state from yield returns,
+        allowing decisions based on current state.
+        """
+
 
     def __add__(self, other: BuildingBlock) -> CombinedBlock:
         return CombinedBlock(blocks=(self, other))
@@ -47,3 +58,8 @@ class CombinedBlock(BuildingBlock):
                     blueprint = yield diff
             except StopIteration:
                 pass
+
+    def _get_change(
+        self, blueprint: Blueprint
+    ) -> Blueprint:
+        raise ValueError(f"{self._get_change.__name__} ")
