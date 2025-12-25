b15ce6620c939b9b1a26b607bac5166255ed3ff8
Tesla2000 <fratajczak124@gmail.com>
2025-12-25 22:09:31 +0100

    Added usage example

---
+++ dnd_character_creator/server/example_generators/example_building_blocks.py
@@ -0,0 +1,139 @@
+from dnd_character_creator.character.blueprint.building_blocks import (
+    CombinedBlock,
+)
+from dnd_character_creator.character.blueprint.building_blocks import (
+    LevelAssigner,
+)
+from dnd_character_creator.character.blueprint.building_blocks import (
+    PriorityStatChoiceResolver,
+)
+from dnd_character_creator.character.blueprint.building_blocks import (
+    RaceAssigner,
+)
+from dnd_character_creator.character.blueprint.building_blocks import (
+    RandomFeatChoiceResolver,
+)
+from dnd_character_creator.character.blueprint.building_blocks import (
+    RandomInitialDataFiller,
+)
+from dnd_character_creator.character.blueprint.building_blocks import (
+    RandomLanguageChoiceResolver,
+)
+from dnd_character_creator.character.blueprint.building_blocks import (
+    RandomSkillChoiceResolver,
+)
+from dnd_character_creator.character.blueprint.building_blocks import (
+    RandomSkillProficiencyChoiceResolver,
+)
+from dnd_character_creator.character.blueprint.building_blocks import (
+    RandomToolProficiencyChoiceResolver,
+)
+from dnd_character_creator.character.blueprint.building_blocks.all_choices_resolver import (
+    AllChoicesResolver,
+)
+from dnd_character_creator.character.blueprint.building_blocks.equipment_chooser import (
+    RandomEquipmentChooser,
+)
+from dnd_character_creator.character.blueprint.building_blocks.feat_choice_resolver.max_first import (
+    MaxFirstResolver,
+)
+from dnd_character_creator.character.blueprint.building_blocks.initial_builder import (
+    InitialBuilder,
+)
+from dnd_character_creator.character.blueprint.building_blocks.level_up.health_increase import (
+    HealthIncreaseAverage,
+)
+from dnd_character_creator.character.blueprint.building_blocks.level_up.level_incrementer import (
+    LevelIncrementer,
+)
+from dnd_character_creator.character.blueprint.building_blocks.level_up.level_up import (
+    LevelUp,
+)
+from dnd_character_creator.character.blueprint.building_blocks.level_up.level_up_multiple import (
+    LevelUpMultiple,
+)
+from dnd_character_creator.character.blueprint.building_blocks.level_up.spell_assignment import (
+    RandomSpellAssigner,
+)
+from dnd_character_creator.character.blueprint.building_blocks.stats_builder.standar_array import (
+    StandardArray,
+)
+from dnd_character_creator.character.blueprint.building_blocks.subclass_assigner import (
+    RandomSubclassAssigner,
+)
+from dnd_character_creator.character.race.race import Race
+from dnd_character_creator.character.race.subraces import Subrace
+from dnd_character_creator.choices.class_creation.character_class import Class
+from dnd_character_creator.choices.stats_creation.statistic import Statistic
+
+
+def example_building_blocks():
+    stats_priority = (
+        Statistic.INTELLIGENCE,
+        Statistic.CONSTITUTION,
+        Statistic.WISDOM,
+        Statistic.DEXTERITY,
+        Statistic.CHARISMA,
+        Statistic.STRENGTH,
+    )
+    class_ = Class.WIZARD
+    all_choices_resolver = AllChoicesResolver(
+        blocks=(
+            RandomLanguageChoiceResolver(),
+            RandomSkillProficiencyChoiceResolver(),
+            MaxFirstResolver(
+                priority=stats_priority,
+                then=RandomFeatChoiceResolver(),
+            ),
+            RandomToolProficiencyChoiceResolver(),
+            PriorityStatChoiceResolver(priority=stats_priority),
+            RandomSkillChoiceResolver(),
+            RandomEquipmentChooser(),
+        ),
+    )
+    spell_assigner = RandomSpellAssigner(class_=class_)
+    level_up = LevelUp(
+        blocks=(
+            LevelIncrementer(class_=class_),
+            HealthIncreaseAverage(class_=class_),
+            spell_assigner,
+            all_choices_resolver,
+        ),
+    )
+    level = 16
+    return CombinedBlock(
+        blocks=(
+            InitialBuilder(
+                blocks=(
+                    LevelAssigner(level=level),
+                    StandardArray(stats_priority=stats_priority),
+                    RaceAssigner(
+                        race=Race.HUMAN,
+                        subrace=Subrace.HUMAN_VARIANT_HUMAN_PLAYERSHANDBOOK,
+                    ),
+                    AllChoicesResolver(
+                        blocks=(
+                            RandomLanguageChoiceResolver(),
+                            RandomSkillProficiencyChoiceResolver(),
+                            MaxFirstResolver(
+                                priority=stats_priority,
+                                then=RandomFeatChoiceResolver(),
+                            ),
+                            RandomToolProficiencyChoiceResolver(),
+                            PriorityStatChoiceResolver(
+                                priority=stats_priority
+                            ),
+                            RandomSkillChoiceResolver(),
+                            RandomEquipmentChooser(),
+                        ),
+                    ),
+                    level_up,
+                )
+            ),
+            RandomInitialDataFiller(),
+            LevelUpMultiple(blocks=tuple(level_up for _ in range(level - 1))),
+            RandomSubclassAssigner(
+                class_=class_,
+            ),
+        )
+    )
